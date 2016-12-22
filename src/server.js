8d691c64a0f1e2b4564520441e1728e1514db86d
sanketmeghani <sanket.meghani@gmail.com>
2016-12-22 12:27:17 +0530

    Adding cliet side scripts

--- src/server.js
+++ src/server.js
@@ -2,6 +2,8 @@ let http = require('http');
 let socketio = require('socket.io');
 let fs = require('fs');
 
+let clientSockets = [];
+
 let clientHandler = (request, response) => {
 
     fs.readFile(__dirname + '/../index.html', (error, data) => {
@@ -14,9 +16,22 @@ let clientHandler = (request, response) => {
         response.writeHead(200);
         response.end(data);
     });
-}
+};
+
+let pushNotification = (notification) => {
+
+    console.log('Pushing notification', JSON.stringify(notification));
+    clientSockets.map((socket) => socket.emit('server-notification', notification));
+};
 
 let server = http.createServer(clientHandler);
+let io = socketio.listen(server);
+
+io.sockets.on('connection', (socket) => {
+    
+    socket.on('push', pushNotification);
+    clientSockets.push(socket);
+});
 
 server.listen(9090);
 console.log('Server started on 9090');
